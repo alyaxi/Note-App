7d838f1f5f76343706aeb9917acd74575bb3bf91
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2020-12-04 06:46:31 -0800

    updated Card

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -10,7 +10,7 @@ export default function Cards(props) {
         <div className="card">
             <div className="face face1">
                 <div className="content">
-                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"/>
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true" alt="img"/>
                     <h3>{props.titlename}</h3>
                 </div>
             </div>
